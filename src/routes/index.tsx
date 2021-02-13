76bca04bfcddbb06e1c278439b103ebd3b01ca3e
LucasGuerraCavalcante <lucasguerratee@gmail.com>
2021-02-12 21:48:20 -0300

    first commit

---
+++ src/routes/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+// import LandingPage from './pages/LandingPage';
+// import ProductsCatalog from './pages/ProductsCatalog';
+// import ProductPage from './pages/ProductPage';
+// import ShoppingCart from './pages/ShoppingCart';
+
+function Routes() {
+    return (
+        <div>
+            <p>a</p>
+        </div>
+        // <BrowserRouter>
+        //     <Switch>
+        //         <Route exact path="/" component={Landing} /> 
+        //         <Route path="/app" component={OrphanagesMap} />  
+
+        //         <Route path="/orphanage/:id" component={Orphanage} />   
+        //         <Route path="/create" component={CreateOrphanage} />   
+        //     </Switch>
+        // </BrowserRouter>
+    );
+}
+
+export default Routes;
