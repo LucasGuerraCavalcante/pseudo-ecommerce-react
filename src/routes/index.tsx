bb22d336b08e9739a1ab6f00b17a877ef364f957
LucasGuerraCavalcante <lucasguerratee@gmail.com>
2021-02-13 01:32:09 -0300

    importing new route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,18 +3,15 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import LandingPage from '../pages/LandingPage';
 import ProductsCatalog from '../pages/ProductsCatalog';
-// import ProductPage from '../pages/ProductPage';
-// import ShoppingCart from '../pages/ShoppingCart';
+import ShoppingCart from '../pages/ShoppingCart';
 
 function Routes() {
     return (
         <BrowserRouter>
             <Switch>
                 <Route exact path="/" component={LandingPage} /> 
-                <Route path="/app" component={ProductsCatalog} />  
-
-                {/* <Route path="/orphanage/:id" component={ProductPage} />    */}
-                {/* <Route path="/create" component={ShoppingCart} />    */}
+                <Route path="/app" component={ProductsCatalog} />    
+                <Route path="/shop" component={ShoppingCart} />   
             </Switch>
         </BrowserRouter>
     );
