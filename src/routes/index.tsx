0b664eecc513e8302a34e53575829d9fc3aa9939
LucasGuerraCavalcante <lucasguerratee@gmail.com>
2021-02-12 22:23:40 -0300

    landing page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,25 +1,22 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-// import LandingPage from './pages/LandingPage';
+import LandingPage from '../pages/LandingPage';
 // import ProductsCatalog from './pages/ProductsCatalog';
 // import ProductPage from './pages/ProductPage';
 // import ShoppingCart from './pages/ShoppingCart';
 
 function Routes() {
     return (
-        <div>
-            <p>a</p>
-        </div>
-        // <BrowserRouter>
-        //     <Switch>
-        //         <Route exact path="/" component={Landing} /> 
-        //         <Route path="/app" component={OrphanagesMap} />  
+        <BrowserRouter>
+            <Switch>
+                <Route exact path="/" component={LandingPage} /> 
+                {/* <Route path="/app" component={ProductsCatalog} />  
 
-        //         <Route path="/orphanage/:id" component={Orphanage} />   
-        //         <Route path="/create" component={CreateOrphanage} />   
-        //     </Switch>
-        // </BrowserRouter>
+                <Route path="/orphanage/:id" component={ProductPage} />   
+                <Route path="/create" component={ShoppingCart} />    */}
+            </Switch>
+        </BrowserRouter>
     );
 }
 
