859ea7a5c0f14cba80e8ad26648703b6c842528a
LucasGuerraCavalcante <lucasguerratee@gmail.com>
2021-02-12 22:48:26 -0300

    importing catalog page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,19 +2,19 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import LandingPage from '../pages/LandingPage';
-// import ProductsCatalog from './pages/ProductsCatalog';
-// import ProductPage from './pages/ProductPage';
-// import ShoppingCart from './pages/ShoppingCart';
+import ProductsCatalog from '../pages/ProductsCatalog';
+// import ProductPage from '../pages/ProductPage';
+// import ShoppingCart from '../pages/ShoppingCart';
 
 function Routes() {
     return (
         <BrowserRouter>
             <Switch>
                 <Route exact path="/" component={LandingPage} /> 
-                {/* <Route path="/app" component={ProductsCatalog} />  
+                <Route path="/app" component={ProductsCatalog} />  
 
-                <Route path="/orphanage/:id" component={ProductPage} />   
-                <Route path="/create" component={ShoppingCart} />    */}
+                {/* <Route path="/orphanage/:id" component={ProductPage} />    */}
+                {/* <Route path="/create" component={ShoppingCart} />    */}
             </Switch>
         </BrowserRouter>
     );
